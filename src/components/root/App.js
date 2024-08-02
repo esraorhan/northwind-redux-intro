bdae123fee730537acb43955afb353f2a3971c99
esraorhan <esraorhan333@gmail.com>
2024-08-02 10:01:18 +0300

    Klasör yapısının oluşturulması

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <div className="App">
-    
+    <Container>
+      <Navi/>
+      <Dashboard/>
+    </Container>
     </div>
   );
 }

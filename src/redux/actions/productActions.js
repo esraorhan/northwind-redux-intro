0d7bdf9a8b4461115a96b318675b164b414dfa3e
esraorhan <esraorhan333@gmail.com>
2024-08-13 21:50:04 +0300

    savedReducer writed

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,6 +4,52 @@ export function getProductsSuccess(products) {
   return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products };
 }
 
+export function createProductsSuccess(product) {
+  return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function updateProductsSuccess(product) {
+  return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductsSuccess(savedProduct))
+          : dispatch(createProductsSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export async function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+
+  const error = await response.text();
+  throw new Error(error);
+}
+
+export function handleError(error) {
+  console.log("bir hata oluştu");
+  throw error;
+}
+
 export function getProducts(categoryId) {
   //apiye bağlanma gibi , veritababnın a bağlanma gibi durumlarda redux thunk gibi bir yaoı çıkıyor.
   return function (dispatch) {
